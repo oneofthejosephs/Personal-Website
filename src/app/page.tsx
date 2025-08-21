f811cd2450f47243f1a92f3af1db8709fd7f7e28
oneofthejosephs <joseph.umoren.12@gmail.com>
2025-08-21 09:53:07 -0400

    Display portfolio properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
+  const Portfolio = require("@/components/Portfolio").default;
+  return <Portfolio />;
+}
+
+/*export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -100,4 +105,4 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+}*/
